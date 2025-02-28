486d3f466ca64fa00cdd926d6db25f31bcb879b1
cmauri-lg <cmauri.lg@gmail.com>
2025-02-27 20:58:11 -0600

    Añadiendo todos los archivos

---
+++ src/main/java/sv/edu/udb/DesafioPractico1Application.java
@@ -0,0 +1,13 @@
+package sv.edu.udb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPractico1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPractico1Application.class, args);
+	}
+
+}
